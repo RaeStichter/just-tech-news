aa03953ab92ac3c44b44b2f3b414d317ed389879
Rae Stichter <raestichter@gmail.com>
2021-02-13 13:53:49 -0500

    updated homepage functionality with handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-
+// Added 14
+const homeRoutes = require('./home-routes.js');
+// end added 14
 const apiRoutes = require('./api');
 
+//added 14
+router.use('/', homeRoutes);
+// end added 14
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// Added in 14
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+// end added in 14
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); //14.1
+// Added in 14
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// end added in 14
 
 // turn on routes
 app.use(routes);
