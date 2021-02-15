131a03aacefef486d113f78f9cc863893c4968ca
Rae Stichter <raestichter@gmail.com>
2021-02-14 23:35:50 -0500

    added editing and deleting functionality. added dashboard for logged in users.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,17 +3,14 @@ const { User, Post, Vote, Comment } = require("../../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll()
-      .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedId = true;
-
-          res.join(dbUserData);
-        });
-      })
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // GET /api/users/1
@@ -28,7 +25,6 @@ router.get('/:id', (req, res) => {
         model: Post,
         attributes: ['id', 'title', 'post_url', 'created_at']
       },
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'created_at'],
@@ -45,16 +41,16 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
@@ -66,7 +62,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -105,13 +109,12 @@ router.post('/login', (req, res) => {
 
 // added in 14
 // allows the user to log out
-router.put('./logout', (req,res) => {
-  if (req.session.loggedId) {
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  }
-  else {
+  } else {
     res.status(404).end();
   }
 });
@@ -128,7 +131,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 // Added 14
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 // end added 14
 const apiRoutes = require('./api');
 
 //added 14
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 // end added 14
 router.use('/api', apiRoutes);
 
